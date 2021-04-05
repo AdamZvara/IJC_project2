f049e88aef637ad98409c829f0c7666b10b72820
Adam Zvara <xzvara01@stud.fit.vutbr.cz>
2021-04-05 21:34:44 +0200

    Remove testing library

--- tests/build/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adam/ijc/project/tests/build/googletest-master
-# Build directory: /home/adam/ijc/project/tests/build/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
