bbb4f568cc38fd1d5078c1f7f3fab0d13275a0b1
Adam Zvara <xzvara01@stud.fit.vutbr.cz>
2021-04-06 20:25:27 +0200

    Working version 1
    Wordcount is now functional - needs further testing

--- wordcount.c
+++ wordcount.c
@@ -4,24 +4,47 @@
 #include "htab.h"
 #include "io.h"
 
-//calculated by the number of words in vocabulary of an average person (20000-35000)
+//calculated by the number of words in vocabulary of an average person 
+//(lower value close to 20000)
 //data taken from site https://wordcounter.io/blog/how-many-words-does-the-average-person-know/
-//taken average value - 27500 and added to the closest prime number
-#define HTAB_SIZE 27509
+//added to a prime number 
+#define HTAB_SIZE 20011
 #define MAX_WORD_LEN 127
+
+void write_pair(htab_pair_t *data)
+{
+    printf("%s %d\n", data->key, data->value);
+}
 
 int main(int argc, char *argv[])
 {
     //TODO general file opening
     (void) argc; (void) argv;
     
-    bool limit_reached = false;
-
     //TODO file opening was unsuccessful
-    FILE *fr = fopen("short_text", "r");
-    char *word = malloc(MAX_WORD_LEN);
+    FILE *fr = fopen("/usr/share/dict/words", "r");
     
+    bool limit_reached = false;
     int length;
+    
+    htab_t *table = htab_init(HTAB_SIZE);
+    if (table == NULL)
+    {
+        fprintf(stderr, "Alokacia tabulky neprebehla uspesne\n");
+        fclose(fr);
+        return 1;
+    }
+
+    char *word = malloc(MAX_WORD_LEN);
+    if (word == NULL)
+    {
+        htab_free(table);
+        fprintf(stderr, "Alokacia slova neprebehla uspesne\n");
+        fclose(fr);
+        return 1;
+    }
+
+    htab_pair_t *pair;
     while ((length = read_word(word, MAX_WORD_LEN, fr)) != EOF)
     {
         if (length == 0 && !limit_reached)
@@ -29,8 +52,23 @@ int main(int argc, char *argv[])
             limit_reached = true;
             fprintf(stderr, "Text obsahuje prilis dlhe slova ktore budu skratene\n");
         }
-        printf("%s\n", word);
-        //do stuff with hash table
+        
+        pair = htab_lookup_add(table, word);
+        if (pair == NULL) 
+        {
+            htab_free(table);
+            free(word);
+            fprintf(stderr, "Alokacia prvku tabulky neprebehla uspesne\n");
+            fclose(fr);
+            return -1;
+        }
+
+        pair->value++;
     }
+
+    htab_for_each(table, (*write_pair));
+
+    htab_free(table);
+    fclose(fr);
     return 0;
 }
